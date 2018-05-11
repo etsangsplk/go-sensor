7b0bebe4a3be160767fccc85baa6786fbea5bfcc
Edward Tsang <etsang@splunk.com>
2018-05-11 16:18:53 -0700

    Lock and test

--- logging/logger.go
+++ logging/logger.go
@@ -172,7 +172,7 @@ func (l *Logger) With(fields ...interface{}) *Logger {
 
 // Lock is an convenient function to convert from generic golang io.writer.
 func Lock(w io.Writer) WriteSyncer {
-	// Use NoOp Sync for protection.
+	// AddSync tries to intelligently convert to right tye for Lock
 	writer := zapcore.AddSync(w)
 	return zapcore.Lock(writer)
 }
